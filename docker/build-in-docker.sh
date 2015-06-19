1a9d76752738624b620bc5de6cc00b0fcc830324
tsaikd <tsaikd@gmail.com>
2015-06-19 15:56:21 +0800

    add build script
    Signed-off-by: tsaikd <tsaikd@gmail.com>

---
+++ docker/build-in-docker.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -e
+
+PN="${BASH_SOURCE[0]##*/}"
+PD="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+renice 15 $$
+cd "${PD}/.."
+
+if ! type godep &>/dev/null ; then
+	go get -v "github.com/tools/godep"
+fi
+
+go get -v
+
+godep restore
+
+if [ "${BUILDTIME}" ] && [ "${GITHASH}" ] ; then
+	go build -ldflags "-X github.com/tsaikd/KDGoLib/version.BUILDTIME ${BUILDTIME} -X github.com/tsaikd/KDGoLib/version.GITCOMMIT ${GITHASH}" -o "godson-$(uname -s)-$(uname -m)"
+else
+	go build -o "godson-$(uname -s)-$(uname -m)"
+fi
+
